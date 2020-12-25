2b00adc6ceb2a4105ec9c2f5568be7c040ec38ff
pjeetb <60028752+pjeetb@users.noreply.github.com>
2020-12-25 20:53:37 +0530

    Reeborg's world
    using the while loops and if statements we will use the Reeborg's world.

---
+++ Day 6: Escaping the Maze/main.py
@@ -0,0 +1,16 @@
+#this code will run in reeborg's world.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+       
