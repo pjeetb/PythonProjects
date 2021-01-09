e15e0feb8efe236439fff0a5b918c3ab519cab6a
pjeetb <60028752+pjeetb@users.noreply.github.com>
2021-01-09 18:11:16 +0530

    file 1

---
+++ Day 13: Debugging practice./debug1.py
@@ -0,0 +1,7 @@
+#Here the int datatype is used to cast the input enterned by the user as string.
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:  #comparison is used interms of assignment operator.
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
